878a05f7537b514dd5953eb63d9eff21140c3600
tubs <tuxiansheng9527@163.com>
2025-07-01 09:54:18 +0800

    feat✨: 环境变量配置&ui引入

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
 import { onLoad, onShow } from '@dcloudio/uni-app'
 import { ref } from 'vue'
+
+const queryValue = ref('')
 </script>
 
 <template>
-  <view class="headText">uniapp tempalte vue3</view>
+  <wd-notice-bar text="仅供娱乐，不知去向何处，做什么，吃什么，加入想法，随机吧" prefix="warn-bold" />
+  <div class="box">
+    <wd-input v-model="queryValue" type="text" placeholder="查询标签" />
+    <wd-button>新增</wd-button>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 page {
   padding: 30rpx;
+}
+.box {
+  padding: 20rpx;
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,65 @@
-import { defineConfig, loadEnv, type UserConfig } from 'vite'
+import { defineConfig, loadEnv, createLogger, type UserConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 import eslintPlugin from 'vite-plugin-eslint'
+import fs from 'fs'
+import path from 'path'
 
-import Components from 'unplugin-vue-components/vite'
-import { VantResolver } from 'unplugin-vue-components/resolvers'
+/**
+ * @description 动态修改 manifest.json
+ * @return {*}
+ */
+class Manifest {
+  private manifestPath: string = './src/manifest.json'
+  private manifest: string
 
-// https://vitejs.dev/config/
+  constructor() {
+    this.manifest = fs.readFileSync(path.join(__dirname, this.manifestPath), { encoding: 'utf-8' })
+  }
+
+  replace(path: string, value: string) {
+    const arr = path.split('.')
+    const len = arr.length
+    const lastItem = arr[len - 1]
+
+    let i = 0
+    const ManifestArr = this.manifest.split(/\n/)
+
+    for (let index = 0, _len = ManifestArr.length; index < _len; index++) {
+      const item = ManifestArr[index]
+      if (new RegExp(`"${arr[i]}"`).test(item)) ++i
+      if (i === len) {
+        const hasComma = /,/.test(item)
+        ManifestArr[index] = item.replace(
+          new RegExp(`"${lastItem}"[\\s\\S]*:[\\s\\S]*`),
+          `"${lastItem}": ${value}${hasComma ? ',' : ''}`
+        )
+        break
+      }
+    }
+
+    this.manifest = ManifestArr.join('\n')
+
+    createLogger().info(`[manifest] set ${path} = ${value}`)
+  }
+
+  write() {
+    fs.writeFileSync(path.join(__dirname, this.manifestPath), this.manifest, {
+      flag: 'w'
+    })
+
+    createLogger().info('[manifest] write config success !')
+  }
+}
+
 export default defineConfig(({ command, mode }) => {
-  const root = process.cwd()
+  console.log('mode ======>', mode)
+  const env = loadEnv(mode, process.cwd())
 
-  const env = loadEnv(mode, root)
+  // 根据环境变量 向 manifest.json 写入 appid
+  const m = new Manifest()
+  m.replace('mp-weixin.appid', `"${env.VITE_WECHAT_APPID || ''}"`)
+  m.write()
+
   const isBuild = command === 'build'
 
   const server: UserConfig['server'] = {
@@ -24,14 +74,16 @@ export default defineConfig(({ command, mode }) => {
   }
 
   const plugins: UserConfig['plugins'] = [
+    // commonjs(/* options */),
     uni(),
+    // svgLoader(),
     eslintPlugin({
       fix: true
-    }),
-    Components({
-      dts: 'types/components.d.ts',
-      resolvers: [VantResolver()]
     })
+    // Components({
+    //   dts: 'types/components.d.ts',
+    //   resolvers: [VantResolver()]
+    // })
   ]
 
   return {
